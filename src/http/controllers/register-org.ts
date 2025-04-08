4bbf240e472e7ba213b5a39905903c49c56c59b5
hnascx <henrique.nascimento12@hotmail.com>
2025-04-08 11:05:13 -0300

    feat: Create global errors handler

--- src/http/controllers/register-org.ts
+++ src/http/controllers/register-org.ts
@@ -37,7 +37,7 @@ export async function registerOrg(
       return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
