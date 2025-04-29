0b1f550f26b45b8b78fba76e293f4ab1d3d829ca
hnascx <henrique.nascimento12@hotmail.com>
2025-04-29 17:54:30 -0300

    test: Add whatsapp button in get pet profile use case

--- src/use-cases/get-pet-profile.spec.ts
+++ src/use-cases/get-pet-profile.spec.ts
@@ -1,18 +1,34 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
-import { GetPetProfileUseCase } from './get-pet-profile'
 import { beforeEach, describe, expect, it } from 'vitest'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { GetPetProfileUseCase } from './get-pet-profile'
 
 let petsRepository: InMemoryPetsRepository
+let orgsRepository: InMemoryOrgsRepository
 let sut: GetPetProfileUseCase
 
 describe('Get Pet Profile Use Case', () => {
   beforeEach(() => {
     petsRepository = new InMemoryPetsRepository()
-    sut = new GetPetProfileUseCase(petsRepository)
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new GetPetProfileUseCase(petsRepository, orgsRepository)
+
+    orgsRepository.create({
+      id: 'org-1',
+      name: 'Friendly Org',
+      email: 'contact@friendlyorg.com',
+      zip_code: '12345-678',
+      address: '123 Friendly St.',
+      city: 'Friendly City',
+      state: 'Friendly State',
+      whatsapp: '+5511999999999',
+      password_hash: 'hashedpassword',
+      created_at: new Date(),
+    })
   })
 
-  it('should be able to get pet profile', async () => {
+  it('should be able to get pet profile with organization details', async () => {
     const createdPet = await petsRepository.create({
       name: 'Charlie',
       about: '',
@@ -26,11 +42,13 @@ describe('Get Pet Profile Use Case', () => {
       org_id: 'org-1',
     })
 
-    const { pet } = await sut.execute({
+    const { pet, org } = await sut.execute({
       petId: createdPet.id,
     })
 
     expect(pet.name).toEqual('Charlie')
+    expect(org.name).toEqual('Friendly Org')
+    expect(org.whatsappButton).toContain('https://wa.me/')
   })
 
   it('should not be able to get pet profile with wrong id', async () => {
