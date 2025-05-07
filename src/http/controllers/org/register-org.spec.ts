e923c16fe2c81b7955a6431b5711d2ab4b0afb23
hnascx <henrique.nascimento12@hotmail.com>
2025-05-07 13:49:58 -0300

    test: Create E2E test of org register

--- src/http/controllers/org/register-org.spec.ts
+++ src/http/controllers/org/register-org.spec.ts
@@ -1,5 +1,26 @@
-import { expect, test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('testing', () => {
-  expect(true).toBe(true)
+describe('Register org (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register an org', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      name: 'John Doe',
+      email: '6WY5Y@example.com',
+      zipCode: '05885-600',
+      address: 'Rua Teste',
+      whatsapp: '123456789',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
